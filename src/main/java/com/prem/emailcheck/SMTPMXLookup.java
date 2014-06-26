f3d1d8cb0f756fac35eb6f406579819d5bcd2bc1
Prembabu Panchadara <prem@eggheadcreative.com>
2014-06-26 18:49:47 +0530

    Refactored some code

--- src/main/java/com/prem/emailcheck/SMTPMXLookup.java
+++ src/main/java/com/prem/emailcheck/SMTPMXLookup.java
@@ -37,7 +37,6 @@ public class SMTPMXLookup {
       }
       if (line.charAt(3) != '-') break;
     }
-
     return res;
   }
 
@@ -66,15 +65,11 @@ public class SMTPMXLookup {
         throw new NamingException
             ("No match for name '" + hostName + "'");
     }
-    // Huzzah! we have machines to try. Return them as an array list
-    // NOTE: We SHOULD take the preference into account to be absolutely
-    //   correct. This is left as an exercise for anyone who cares.
-    ArrayList res = new ArrayList();
-    NamingEnumeration en = attr.getAll();
-
-    while (en.hasMore()) {
+    ArrayList resultList = new ArrayList();
+    NamingEnumeration enumeration = attr.getAll();
+    while (enumeration.hasMore()) {
       String mailhost;
-      String x = (String) en.next();
+      String x = (String) enumeration.next();
       String f[] = x.split(" ");
       //  THE fix *************
       if (f.length == 1)
@@ -83,17 +78,14 @@ public class SMTPMXLookup {
         mailhost = f[1].substring(0, (f[1].length() - 1));
       else
         mailhost = f[1];
-      res.add(mailhost);
+      resultList.add(mailhost);
     }
-    return res;
+    return resultList;
   }
 
   public static boolean isAddressValid(String address) {
-    // Find the separator for the domain name
     int pos = address.indexOf('@');
-    // If the address does not contain an '@', it's not valid
     if (pos == -1) return false;
-    // Isolate the domain/machine name and get a list of mail exchangers
     String domain = address.substring(++pos);
     ArrayList mxList = null;
     try {
@@ -101,14 +93,7 @@ public class SMTPMXLookup {
     } catch (NamingException ex) {
       return false;
     }
-    // Just because we can send mail to the domain, doesn't mean that the
-    // address is valid, but if we can't, it's a sure sign that it isn't
     if (mxList.size() == 0) return false;
-    // Now, do the SMTP validation, try each mail exchanger until we get
-    // a positive acceptance. It *MAY* be possible for one MX to allow
-    // a message [store and forwarder for example] and another [like
-    // the actual mail server] to reject it. This is why we REALLY ought
-    // to take the preference into account.
     for (int mx = 0; mx < mxList.size(); mx++) {
       boolean valid = false;
       try {
@@ -118,23 +103,17 @@ public class SMTPMXLookup {
             (new InputStreamReader(skt.getInputStream()));
         BufferedWriter wtr = new BufferedWriter
             (new OutputStreamWriter(skt.getOutputStream()));
-
         res = hear(rdr);
         if (res != 220) throw new Exception("Invalid header");
         say(wtr, "EHLO rgagnon.com");
-
         res = hear(rdr);
         if (res != 250) throw new Exception("Not ESMTP");
-
         // validate the sender address
         say(wtr, "MAIL FROM: <tim@orbaker.com>");
         res = hear(rdr);
         if (res != 250) throw new Exception("Sender rejected");
-
         say(wtr, "RCPT TO: <" + address + ">");
         res = hear(rdr);
-
-        // be polite
         say(wtr, "RSET");
         hear(rdr);
         say(wtr, "QUIT");
@@ -146,7 +125,6 @@ public class SMTPMXLookup {
         wtr.close();
         skt.close();
       } catch (Exception ex) {
-        // Do nothing but try next host
         ex.printStackTrace();
       } finally {
         if (valid) {
